94562abd743b154781f61700ce0a225def14017b
Franklyn <franklynwisdom@gmail.com>
2021-07-15 23:37:35 +0100

    Built the meme generator app in react

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+
+const root =document.getElementById('root');
+
+ReactDOM.render(
+<App/>,
+root);
+
+reportWebVitals();
+
+
+
+
+
